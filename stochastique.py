82ef9f44b5acb37c0d5bd8d1077fa640328c53b4
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2025-01-06 09:50:35 +0100

    sto

--- stochastique.py
+++ stochastique.py
@@ -4,19 +4,13 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from descente_stochastique import GradientDescent
-from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
 def sigmoid(z):
     z = np.clip(z, -500, 500)  # Limite les valeurs pour éviter le débordement
     return 1 / (1 + np.exp(-z))
 
-def compute_gradient(weights, x_batch, y_batch):
-    m = x_batch.shape[0]
-    predictions = sigmoid(np.dot(x_batch, weights))
-    errors = predictions - y_batch
-    gradient = np.dot(x_batch.T, errors) / m
-    return gradient
 
 # Charger les données digits
 digits = load_digits()
@@ -30,12 +24,12 @@ X = scaler.fit_transform(X)
 X = np.hstack((np.ones((X.shape[0], 1)), X))
 
 # Partition des données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialisation des paramètres
 num_classes = len(np.unique(y))
 initial_weights = np.zeros((X_train.shape[1], num_classes))  # Poids pour chaque classe
-learning_rate = 0.1
+learning_rate = 0.01
 max_iterations = 500
 
 def wrapped_gradient(weights, batch):
@@ -48,7 +42,8 @@ def wrapped_gradient(weights, batch):
     return gradient
 
 data = np.hstack((X_train, y_train.reshape(-1, 1)))
-gd = GradientDescent(gradient=wrapped_gradient, learning_rate=learning_rate, max_iterations=max_iterations, batch_size=32)
+print(data)
+gd = GradientDescent(gradient=wrapped_gradient, learning_rate=learning_rate, max_iterations=max_iterations, batch_size=1)
 
 # Entraînement
 weights = gd.descent(initial_weights, data)
@@ -66,7 +61,7 @@ plt.title("Matrice de confusion")
 plt.show()
 
 # Affichage des prédictions pour quelques chiffres
-fig, axes = plt.subplots(5, 5, figsize=(10, 10))
+fig, axes = plt.subplots(5, 5, figsize=(8, 8))
 for i, ax in enumerate(axes.ravel()):
     ax.imshow(X_test[i, 1:].reshape(8, 8), cmap='gray')
     ax.axis('off')
